e6b1a23e7fbf2b5176dcdf175d9b74ac74bc19a7
Tessa <Tessachi33@yahoo.com>
2015-08-28 10:43:22 -0700

    finds sylist in database, passing tests one by one

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -41,5 +41,54 @@ public static List<Stylist> all() {
        return con.createQuery(sql).executeAndFetch(Stylist.class);
      }
    }
+public void save() {
+  try(Connection con = DB.sql2o.open()) {
+    String sql ="INSERT INTO stylist (name, client_id) VALUES (:name, :client_id)";
+    this.id = (int) con.createQuery(sql, true)
+    .addParameter("name", this.name)
+    .addParameter("client_id", this.client_id)
+    .executeUpdate()
+    .getKey();
+  }
+}
+public static Stylist find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM stylist WHERE id=:id";
+      Stylist stylist = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Stylist.class);
+      return stylist;
+    }
+  }
+
+  public void update(String name) {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "UPDATE stylist SET name = :name WHERE id=:id";
+    con.createQuery(sql)
+      .addParameter("id", id)
+      .addParameter("name", name)
+      .executeUpdate();
+  }
+}
+//
+// public String getClientName() {
+//   try(Connection con = DB.sql2o.open()){
+//     String sql = "SElECT name FROM client WHERE id = :id;";
+//     return con.createQuery(sql)
+//     .addParameter("id", client_id)
+//     .executeAndFetchFirst(String.class);
+//   }
+// }
+//
+// public void delete() {
+//   try(Connection con = DB.sql2o.open()){
+//     String sql = "DELETE FROM stylist WHERE id = :id;";
+//     con.createQuery(sql)
+//     .addParameter("id", id)
+//     .executeUpdate();
+//   }
+// }
+
+
 
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -11,4 +11,64 @@ public class StylistTest {
   public void all_emptyAtFirst() {
    assertEquals(Stylist.all().size(), 0);
   }
+
+  @Test
+   public void equals_returnsTrueIfNameAreTheSame() {
+    Stylist firstStylist = new Stylist("Kelly", 1);
+    Stylist secondStylist = new Stylist("Kelly", 1);
+    assertTrue(firstStylist.equals(secondStylist));
+    }
+
+  @Test
+    public void save_saveObjectIntoDatabase_true() {
+     Stylist myStylist = new Stylist("Kelly", 1);
+     myStylist.save();
+     assertTrue(Stylist.all().get(0).equals(myStylist));
+     }
+
+  @Test
+    public void save_assignsIdtoObject() {
+     Stylist myStylist = new Stylist("Kelly", 1);
+     myStylist.save();
+     Stylist savedStylist = Stylist.all().get(0);
+     assertEquals(myStylist.getId(), savedStylist.getId());
+     }
+
+  @Test
+   public void find_findsStylistInDatabase_true() {
+     Stylist myStylist = new Stylist("Kelly", 1);
+     myStylist.save();
+     //Stylist savedStylist = Stylist.all().get(0);
+     Stylist savedStylist= Stylist.find(myStylist.getId());
+     assertTrue(myStylist.equals(savedStylist));
+     }
+
+      //  @Test
+      //  public void save_savesClient_idIntoDB_true() {
+      //    Client myClient = new Client("jo");
+      //    myClient.save();
+    //      Stylist myStylist = new Stylist("Kelly", 1);
+      //    savedStylist.save();
+        //  Stylist savedStylist= Stylist.find(myStylist.getId());
+      //    assertEquals(savedStylist.getClient_id(), myClient.getId());
+      //  }
+       //
+      //  @Test
+      //  public void update_updateRestaurantInfo() {
+      //      Stylist myStylist = new Stylist("Kelly", 1);
+      //    savedStylist.save();
+      //    savedStylist.update
+  //      Stylist myStylist = new Stylist("makr", 3);
+    //    assertTrue(Stylist.all().get(0).equals(savedStylist));
+      //  }
+       //
+      //  @Test
+      //  public void delete_deleteStylistFromDatabase() {
+      //    Stylist myStylist = new Stylist("kelly", 1);
+      //    myStylist.save();
+      //    myStylist.delete();
+      //    assertEquals(stylist.all().size(), 0);
+       //}
+
+
 }
