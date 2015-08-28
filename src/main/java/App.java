fb9a613d494a7499f03e7b188e8df7a21a11ee3d
Tessa <Tessachi33@yahoo.com>
2015-08-28 11:43:16 -0700

    add first route and add client form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,5 +9,13 @@ public class App {
   public static void main(String[] args) {
     String layout = "templates/layout.vtl";
 
+    get("/", (request, response) -> {
+         Map<String, Object> model = new HashMap<String,Object>();
+
+         model.put("client", Client.all());
+         model.put("template", "templates/home.vtl");
+         model.put("stylist", Stylist.all());
+         return new ModelAndView(model, layout);
+       }, new VelocityTemplateEngine());
  }
 }
